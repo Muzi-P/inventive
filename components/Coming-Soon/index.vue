6d89681aeceb25eb78dfc55b7f53b66824260715
Muzi Gondwe <muzi.gondwe@fariaedu.com>
2023-04-16 11:36:13 +0800

    add: comming soon pag

---
+++ components/Coming-Soon/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="comming-soon grid section-padding bg-img parallaxie" data-overlay-dark="5" data-background="/img/comming-soon.png">
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Coming-Soon",
+}
+</script>
+
+<style scoped>
+.comming-soon  {
+    height: 100vh;
+    background-image: url('/img/soon.png');
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <Navbar ref="navbar" theme="light" />
-    <Home3-Light-Header />
-    <Home3-Light-Services />
-    <Home3-Light-Works />
-    <Home3-Light-About />
-    <Home3-Light-Numbers />
-    <Home3-Light-Testimonials />
-    <Home2-Light-News />
-    <Footer />
+    <Coming-Soon />
   </div>
 </template>
 
@@ -22,19 +14,6 @@ export default {
     }
   },
   mounted() {
-    document.body.classList.remove('index2');
-    document.body.classList.remove('index3');
-    document.body.classList.remove('index4');
-
-    var navbar = this.$refs.navbar.$el;
-
-    if (window.pageYOffset > 300) navbar.classList.add("nav-scroll");
-    else navbar.classList.remove("nav-scroll");
-
-    window.addEventListener("scroll", () => {
-      if (window.pageYOffset > 300) navbar.classList.add("nav-scroll");
-      else navbar.classList.remove("nav-scroll");
-    });
   }
 }
 </script>
